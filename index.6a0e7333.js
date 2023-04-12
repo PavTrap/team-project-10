47f556eea13b606b01721bcd4ba8bd1110bea83c
PavTrap <105560877+PavTrap@users.noreply.github.com>
2023-04-12 15:06:29 +0000

    Deploying to gh-pages from @ PavTrap/team-project-10@91b7554461f04d24b94bad4f854eeab962a502c1 🚀

---
+++ index.6a0e7333.js
@@ -0,0 +1,2 @@
+!function(){var e={categoriesList:document.querySelector(".categories__list")};e.categoriesList.style.listStyle="none";fetch("https://books-backend.p.goit.global/books/category-list").then((function(e){return e.json()})).then((function(t){var o=t.map((function(e){var t=e.list_name;return'<li class = "categories__item"><a href = "" class = "categories__link">'.concat(t,"</a></li>")}));return e.categoriesList.insertAdjacentHTML("beforeend",o.join("")),console.log(o),o}))}();
+//# sourceMappingURL=index.6a0e7333.js.map
