40c1f526a41b66028956f128e1ebbe13980ca304
Chris <kisunya750@gmail.com>
2023-04-14 22:00:22 +0300

    bag

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './js/array_foundation';
-import './js/markup-foundation';
 import './js/categories';
 import './shoppinglist';
 import './js/header';
+import './js/markup-foundation';
 import './js/modal-window';
+
