f872d684c7f770bfb847a5550f97352f5fac8b5a
Pavel_soundNotLight <105560877+PavTrap@users.noreply.github.com>
2023-04-15 15:17:22 +0300

    add data categories

--- src/js/categories.js
+++ src/js/categories.js
@@ -7,7 +7,7 @@ const fetchArray = fetch('https://books-backend.p.goit.global/books/category-lis
 .then(response => response.json())
 .then(data => {
 	const array = data.map(({ list_name }) => {
-	return `<li class = "categories__item"><a href = "" class = "categories__link">${list_name}</a></li>`
+	return `<li class = "categories__item"><a href = "" data-category = "${list_name}" class = "categories__link">${list_name}</a></li>`
 	})
 	refs.categoriesList.insertAdjacentHTML('beforeend', array.join(''));
 	console.log(array)
