d061b08adba87665934042ca8c3565a48e197270
Chris <kisunya750@gmail.com>
2023-04-16 13:12:34 +0300

    remove number from arr

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import './js/array_foundation';
-import './js/categories';
-import './shoppinglist';
-import './js/header';
+// import './js/categories';
+// import './shoppinglist';
+// import './js/header';
 
 import './js/markup-foundation';
 
 
-import './js/bestsellers';
+// import './js/bestsellers';
 
 
-import './js/modal-window';
+// import './js/modal-window';
 
