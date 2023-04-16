dc42b52b1158ee75107f877e62f9aebf99c0d6e2
PavTrap <105560877+PavTrap@users.noreply.github.com>
2023-04-16 10:26:41 +0000

    Deploying to gh-pages from @ PavTrap/team-project-10@8a98e183bb7e1b960b611c56437b860e9e67d195 🚀

---
+++ shoppinglist.98535241.js
@@ -0,0 +1,2 @@
+let e=document.querySelector("body").getAttribute("data-current-page");"index"===e?document.querySelector('.nav-link[data-href="index"]').classList.add("nav-link-current"):"shoppinglist"===e&&document.querySelector('.nav-link[data-href="shoppinglist"]').classList.add("nav-link-current");
+//# sourceMappingURL=shoppinglist.98535241.js.map
---
+++ shoppinglist.f081a5d3.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("body").getAttribute("data-current-page");"index"===e?document.querySelector('.nav-link[data-href="index"]').classList.add("nav-link-current"):"shoppinglist"===e&&document.querySelector('.nav-link[data-href="shoppinglist"]').classList.add("nav-link-current")}();
+//# sourceMappingURL=shoppinglist.f081a5d3.js.map
