1fe89f48d5179409d563b9a461d528b13fd432e8
Chris <kisunya750@gmail.com>
2023-04-16 15:52:14 +0300

    Add slider

--- src/js/markup-foundation.js
+++ src/js/markup-foundation.js
@@ -1,56 +1,58 @@
 import { nameFoundation } from './array_foundation'
 
 const foundationListEl = document.querySelector(".foundation__list");
-const sliderBtn = document.querySelector('.foundation__btn');
-const sliderWrapper = document.querySelector('.slider');
-const imgSlides = document.querySelectorAll('.foundation__item');
 
-
-console.log(foundationListEl)
-console.log(nameFoundation.length)
-    function markupList(arr) {
-        return arr.map(({ title, url, img, number }) => {
-            return `<li class= 'foundation__item'> 
-        <p class = "foundation__count"> 0${number} </p>
+ const markupList = arr => {
+    return arr.map((item, index) => {
+      const { title, url, img } = item;
+    const countNumber = (index + 1).toString().padStart(2, '0');
+        
+        return `<li class= 'foundation__item'> 
+        <p class = "foundation__count"> ${countNumber} </p>
         <a href="${url}" "foundation__link">
-        <img src="${img}" alt="${title}" width=110 height=33>
+        <img class="foundation__img" src="${img}" alt="${title}" width=110 height=33>
         </a>
         </li>`
-    })
-        .join('');
+    }).join('');
 }
 
 foundationListEl.innerHTML = markupList(nameFoundation);
 
-
-// let counter = 0;
-// let moveDown = false;
-
-// const verticalSlider = function () {
-//   imgSlides.forEach(function (slide) {
-//     slide.style.transform = `translateY(-${counter * 100}%)`;
-//   });
-// };
-
-// sliderBtn.addEventListener('click', function () {
-//   if (moveDown) {
-//     counter -= 2;
-//     if (counter < 0) {
-//       counter = 0;
-//       moveDown = false;
-//       sliderBtn.querySelector('.support__icon').style.transform = '';
-//     }
-//   } else {
-//     counter += 2;
-//     if (counter >= imgSlides.length) {
-//       counter = imgSlides.length - 1;
-//       moveDown = true;
-//       sliderBtn.querySelector('.support__icon').style.transform = 'rotate(180deg)';
-//     }
-//     }
-    
-//     verticalSlider(markupList);
-// });
+const sliderBtnEl = document.querySelector('.foundation__btn');
+const foundItemsEl = document.querySelectorAll('.foundation__item');
 
 
-  
+// SLIDER
+
+const supportSlider = function () {
+    foundItemsEl.forEach(function (slide) {
+        slide.style.transform = `translateY(-${counter * 40}px)`;
+    });
+}
+
+// BUTTON CLICK
+sliderBtnEl.addEventListener('click', movementSlide);
+
+let counter = 0;
+let moveDown = false;
+
+ function movementSlide () {
+  if (moveDown) {
+    counter -= 3;
+    if (counter < 0) {
+      counter = 0;
+      moveDown = false;
+      sliderBtnEl.querySelector('.btnLoadMore__icon').style.transform = '';
+    }
+  } else {
+    counter += 3;
+      if (counter >= foundItemsEl.length) {
+      counter = foundItemsEl.length - 2;
+      moveDown = true;
+      sliderBtnEl.querySelector('.btnLoadMore__icon').style.transform = 'rotate(270deg)';
+    }
+     }
+    supportSlider();
+}
+
+
