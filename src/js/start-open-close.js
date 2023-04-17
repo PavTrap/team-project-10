591ec23d708947949907edfaedf06f1b2c538678
Olena Zozulia <viktorzozylia@gmail.com>
2023-04-17 11:43:50 +0300

    start-17.04

--- src/js/start-open-close.js
+++ src/js/start-open-close.js
@@ -1,16 +1,18 @@
 (() => {
     const refs = {
-    openModalBtn: document.querySelector("button[data-modal-start-open--sing-up]"),
-    closeModalBtn: document.querySelector("button[data-modal-start-close]"),
-    modal: document.querySelector("[data-modal-start]"),
+    openModalBtnSingUp: document.querySelector("button[data-modal-start-open--sing-up]"),
+    closeModalBtnXClose: document.querySelector("button[data-modal-start-close]"),
+    formEl: document.querySelector("[data-modal-start]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtnSingUp.addEventListener("click", toggleModal);
+  refs.closeModalBtnXClose.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden-start");
+    refs.formEl.classList.toggle("is-hidden-start");
+    refs.formEl.classList.toggle("display");
   }
+
 })();
 
 
