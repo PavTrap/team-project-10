e27b3cd588943524452ed5bd9be1ceac662181df
ToFFeeV <khmiluvkiyv.ku@gmail.com>
2023-04-18 23:01:21 +0300

    Update modal window

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -11,44 +11,42 @@ const fetchUsers = async id => {
   return users;
 };
 
-const amazonPic = new URL(import.meta.url);
-const applebooksPic = new URL(import.meta.url);
-const bookstore_shopPic = new URL(import.meta.url);
+const amazonPic = new URL('../images/shop-icons/amazon.jpg', import.meta.url);
+const applebooksPic = new URL('../images/shop-icons/applebooks.jpg', import.meta.url);
+const bookstore_shopPic = new URL('../images/shop-icons/bookstore.jpg', import.meta.url);
 
 const modalBtnAddRemove = document.querySelector('.pop__btn');
+const modalData = document.getElementById('modal')
+const bookInfoURL = document.getElementById('bookInfoUrl')
+const bookInfo = document.getElementById('bookInfo')
+const bookInform = document.getElementById('bookInform')
 
 // Функція для відкриття модального вікна та заповнення його даними з API
 export function openModal(id) {
-  // const bookInfo = document.getElementById('bookInfo')
+
   const bookId = fetchUsers(id);
   bookId.then(id => {
     bookInfo.insertAdjacentHTML(
       'afterbegin',
-      `<li class="shopping-carg ${id._id}" >
-                    <img class="shopping-carg-image" src="${
-                      id.book_image
-                    }" alt="" />
-                    <div class="">
-                        <div class="">
-                            <div>
-                                <h2 class="shopping-carg-book-title">${
-                                  id.title
-                                }</h2>
-                                <p class="shopping-carg-book-author">${
-                                  id.author
-                                }</p>
-                                <p class="shopping-carg-book-description">${
-                                  id.description || 'There is no description'
-                                }</p>
-                            </div>
-                        </div>
-                    </div>
-                </li>`
+      `<li ${id._id}" >
+        <div class="modal-card__flex">
+          <div class="modal-card">
+          <img class="modal-card__image" src="${id.book_image}" alt="" />
+          
+        </div>
+        
+        </div>
+      </li>`
     );
+    bookInform.insertAdjacentHTML('afterbegin', `<div>
+          <h2 class="modal-card__title">${id.title}</h2>
+          <p class="modal-card__author">${id.author}</p>
+          <p class="modal-card__description">${id.description || 'There is no description'}</p>
+        </div>`)
 
     id.buy_links.map(el => {
       if (el.name === 'Amazon') {
-        bookInfo.insertAdjacentHTML(
+        bookInfoURL.insertAdjacentHTML(
           'beforeend',
           `<li class="pop_shop-item">
                             <a
@@ -57,7 +55,7 @@ export function openModal(id) {
                             target="_blank"
                             rel="noopener noreferrer"
                             ><img
-                                class="pop_list-img"
+                                class="modal_list-img"
                                 src="${amazonPic}" 
                                 alt="amazon_shop_icon"/>
                             </a>
@@ -65,7 +63,7 @@ export function openModal(id) {
         );
       }
       if (el.name === 'Apple Books') {
-        bookInfo.insertAdjacentHTML(
+        bookInfoURL.insertAdjacentHTML(
           'beforeend',
           `<li class="pop_shop-item">
                     <a
@@ -83,7 +81,7 @@ export function openModal(id) {
         );
       }
       if (el.name === 'Bookshop') {
-        bookInfo.insertAdjacentHTML(
+        bookInfoURL.insertAdjacentHTML(
           'beforeend',
           `<li class="pop_shop-item">
                     <a
@@ -108,24 +106,32 @@ export function openModal(id) {
   modal.style.display = 'block';
 
   // Додати обробник події на кнопку закриття модального вікна
-  const closeButton = document.getElementsByClassName('close')[0];
+  const closeButton = document.getElementsByClassName('modal-btn')[0];
   closeButton.onclick = function () {
     modal.style.display = 'none';
     bookInfo.innerHTML = '';
+    // bookInfoURL.innerHTML = '';
+    bookInform.innerHTML = '';
   };
 }
 
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = 'none';
+    // modalData.innerHTML = '';
     bookInfo.innerHTML = '';
+    // bookInfoURL.innerHTML = '';
+    bookInform.innerHTML = '';
   }
 };
 
 window.addEventListener('keydown', event => {
   if (event.key === 'Escape') {
     modal.style.display = 'none';
+    // modalData.innerHTML = '';
     bookInfo.innerHTML = '';
+    // bookInfoURL.innerHTML = '';
+    bookInform.innerHTML = '';
   }
 });
 
