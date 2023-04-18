9ed696702faa925fdd9c87448d9e4d5664648869
Pavel_soundNotLight <105560877+PavTrap@users.noreply.github.com>
2023-04-18 13:48:23 +0300

    add idFormBook

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,40 +1,56 @@
-const fetchUsers = async (id) => {
-    const response = await fetch(`https://books-backend.p.goit.global/books/${id}`);
-    const users = await response.json();
-    console.log(users);
-    return users;
+let idFromBook;
+
+const fetchUsers = async id => {
+  idFromBook = id;
+
+  const response = await fetch(
+    `https://books-backend.p.goit.global/books/${id}`
+  );
+  const users = await response.json();
+  console.log(users);
+  return users;
 };
 
 const amazonPic = new URL(import.meta.url);
 const applebooksPic = new URL(import.meta.url);
 const bookstore_shopPic = new URL(import.meta.url);
 
+const modalBtnAddRemove = document.querySelector('.pop__btn');
 
 // Функція для відкриття модального вікна та заповнення його даними з API
 export function openModal(id) {
-    // const bookInfo = document.getElementById('bookInfo')
-    const bookId = fetchUsers(id);
-    bookId
-        .then(id => {
-            bookInfo.insertAdjacentHTML('afterbegin',
-                `<li class="shopping-carg ${id._id}" >
-                    <img class="shopping-carg-image" src="${id.book_image}" alt="" />
+  // const bookInfo = document.getElementById('bookInfo')
+  const bookId = fetchUsers(id);
+  bookId.then(id => {
+    bookInfo.insertAdjacentHTML(
+      'afterbegin',
+      `<li class="shopping-carg ${id._id}" >
+                    <img class="shopping-carg-image" src="${
+                      id.book_image
+                    }" alt="" />
                     <div class="">
                         <div class="">
                             <div>
-                                <h2 class="shopping-carg-book-title">${id.title}</h2>
-                                <p class="shopping-carg-book-author">${id.author}</p>
-                                <p class="shopping-carg-book-description">${id.description || 'There is no description'}</p>
+                                <h2 class="shopping-carg-book-title">${
+                                  id.title
+                                }</h2>
+                                <p class="shopping-carg-book-author">${
+                                  id.author
+                                }</p>
+                                <p class="shopping-carg-book-description">${
+                                  id.description || 'There is no description'
+                                }</p>
                             </div>
                         </div>
                     </div>
                 </li>`
-            );
+    );
 
-            id.buy_links.map(el => {
-                if (el.name === 'Amazon') {
-                    bookInfo.insertAdjacentHTML('beforeend',
-                        `<li class="pop_shop-item">
+    id.buy_links.map(el => {
+      if (el.name === 'Amazon') {
+        bookInfo.insertAdjacentHTML(
+          'beforeend',
+          `<li class="pop_shop-item">
                             <a
                             class="pop_shop__link link"
                             href="${el.url}"
@@ -46,12 +62,12 @@ export function openModal(id) {
                                 alt="amazon_shop_icon"/>
                             </a>
                         </li>`
-                    );
-                }
-                if (el.name === 'Apple Books') {
-                    bookInfo.insertAdjacentHTML(
-                        'beforeend',
-                        `<li class="pop_shop-item">
+        );
+      }
+      if (el.name === 'Apple Books') {
+        bookInfo.insertAdjacentHTML(
+          'beforeend',
+          `<li class="pop_shop-item">
                     <a
                     class="pop_shop__link link"
                     href="${el.url}"
@@ -64,12 +80,12 @@ export function openModal(id) {
                     />
                     </a>
                 </li>`
-                    );
-                }
-                if (el.name === 'Bookshop') {
-                    bookInfo.insertAdjacentHTML(
-                        'beforeend',
-                        `<li class="pop_shop-item">
+        );
+      }
+      if (el.name === 'Bookshop') {
+        bookInfo.insertAdjacentHTML(
+          'beforeend',
+          `<li class="pop_shop-item">
                     <a
                     class="pop_shop__link link"
                     href="${el.url}"
@@ -83,32 +99,43 @@ export function openModal(id) {
                     </a>
                     </li>
                     `
-                    );
-                }
-            });
-        })
+        );
+      }
+    });
+  });
 
-    // Відобразити модальне вікно
-    modal.style.display = "block";
+  // Відобразити модальне вікно
+  modal.style.display = 'block';
 
-    // Додати обробник події на кнопку закриття модального вікна
-    const closeButton = document.getElementsByClassName("close")[0];
-    closeButton.onclick = function () {
-        modal.style.display = "none";
-        bookInfo.innerHTML = "";
-    };
+  // Додати обробник події на кнопку закриття модального вікна
+  const closeButton = document.getElementsByClassName('close')[0];
+  closeButton.onclick = function () {
+    modal.style.display = 'none';
+    bookInfo.innerHTML = '';
+  };
 }
 
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-        bookInfo.innerHTML = "";
-    }
-}
+  if (event.target == modal) {
+    modal.style.display = 'none';
+    bookInfo.innerHTML = '';
+  }
+};
 
 window.addEventListener('keydown', event => {
-    if (event.key === 'Escape') {
-        modal.style.display = "none";
-        bookInfo.innerHTML = "";
-    }
+  if (event.key === 'Escape') {
+    modal.style.display = 'none';
+    bookInfo.innerHTML = '';
+  }
+});
+
+modalBtnAddRemove.addEventListener('click', event => {
+  const dataFromLocalStorage = localStorage.getItem('User-name');
+  const parsedDataFromLocalStorage = JSON.parse(dataFromLocalStorage);
+  console.log(parsedDataFromLocalStorage.listId);
+  parsedDataFromLocalStorage.listId.push(idFromBook);
+  localStorage.setItem(
+    'User-name',
+    JSON.stringify({ listId: parsedDataFromLocalStorage.listId })
+  );
 });
