980f729ed6c979d881476cc83c3d49ab986dea8d
Pavel_soundNotLight <105560877+PavTrap@users.noreply.github.com>
2023-04-18 20:20:46 +0300

    changed all header and start

--- src/js/start-open-close.js
+++ src/js/start-open-close.js
@@ -1,46 +1,42 @@
-
 (() => {
-    const refs = {
-    openModalBtnSingUp: document.querySelector("button[data-modal-start-open--sing-up]"),
-    closeModalBtnXClose: document.querySelector("button[data-modal-start-close]"),
-    formEl: document.querySelector("[data-modal-start]"),
+  const refs = {
+    openModalBtnSingUp: document.querySelector(
+      'button[data-modal-start-open--sing-up]'
+    ),
+    closeModalBtnXClose: document.querySelector(
+      'button[data-modal-start-close]'
+    ),
+    formEl: document.querySelector('[data-modal-start]'),
   };
 
-  refs.openModalBtnSingUp.addEventListener("click", toggleModal);
-  refs.closeModalBtnXClose.addEventListener("click", toggleModal);
+  refs.openModalBtnSingUp.addEventListener('click', toggleModal);
+  refs.closeModalBtnXClose.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.formEl.classList.toggle("is-hidden-start");
-    refs.formEl.classList.toggle("display");
+    refs.formEl.classList.toggle('is-hidden-start');
+    refs.formEl.classList.toggle('display');
   }
-
 })();
 
 // Sing Up в мобилке
 
-(() => {
-    const refs = {
-    openModalBtnSingUpMobile: document.querySelector("button[data-modal-start-open--sing-up-mobile]"),
-    closeModalBtnXClose: document.querySelector("button[data-modal-start-close]"),
-    formEl: document.querySelector("[data-modal-start-mobile]"),
-  };
+// (() => {
+//     const refs = {
+//     openModalBtnSingUpMobile: document.querySelector("button[data-modal-start-open--sing-up-mobile]"),
+//     closeModalBtnXClose: document.querySelector("button[data-modal-start-close]"),
+//     formEl: document.querySelector("[data-modal-start-mobile]"),
+//   };
 
-  refs.openModalBtnSingUpMobile.addEventListener("click", toggleModal);
-  refs.closeModalBtnXClose.addEventListener("click", toggleModal);
+//   refs.openModalBtnSingUpMobile.addEventListener("click", toggleModal);
+//   refs.closeModalBtnXClose.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.formEl.classList.toggle("is-hidden-start-mobile");
-  }
+//   function toggleModal() {
+//     refs.formEl.classList.toggle("is-hidden-start-mobile");
+//   }
 
-})();
+// })();
 
-refs.openModalBtnSingUpMobile.addEventListener('click', e => {
-  formEl.style.display = "block";
-  formEl.style.zIndex = "5";
-})
-
-
-
-
-
-
+// refs.openModalBtnSingUpMobile.addEventListener('click', e => {
+//   formEl.style.display = "block";
+//   formEl.style.zIndex = "5";
+// })
